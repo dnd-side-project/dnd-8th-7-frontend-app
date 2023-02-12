b08d123691ab5e2d54ecced1ea978cbcf26735ed
eun-seong <eunseong.dev@gmail.com>
2023-02-13 00:35:33 +0900

    chore: 디렉토리 alias 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          '@/assets': './src/assets',
+          '@/components': './src/components',
+          '@/navigations': './src/navigations',
+          '@/screens': './src/screens',
+          '@/styles': './src/styles',
+        },
+      },
+    },
+  },
 };
