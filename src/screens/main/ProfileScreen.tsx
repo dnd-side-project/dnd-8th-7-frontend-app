50ae071d8345dd4cd7ab01135b20ae4971e13d80
eunseonglee <eunseongofficial@gmail.com>
2023-02-17 02:13:31 +0900

    feat: 메인의 스크린 파일 생성(#8)

---
+++ src/screens/main/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import CustomSafeAreaView from '@/components/CustomSafeAreaView';
+import {Text} from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+    <CustomSafeAreaView>
+      <Text>profile screen</Text>
+    </CustomSafeAreaView>
+  );
+}
